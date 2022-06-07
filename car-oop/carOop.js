73a5b92a853bf651ca280b3ad7d83d924cfd186b
Jeremy Tanner <jtannerdev@gmail.com>
2022-06-07 14:25:36 -0400

    car OOP

---
+++ car-oop/carOop.js
@@ -0,0 +1,38 @@
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  honk() {
+    return "Beep.";
+  }
+  toString() {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+
+  revEngine() {
+    return "VROOM!!!";
+  }
+}
+
+class Garage {
+  constructor(amount) {
+    this.vehicles = [];
+    this.capacity = amount;
+  }
+}
